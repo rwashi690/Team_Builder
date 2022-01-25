1d082fb5dcf72eeab96f0b870234fb4753369332
rwashi690 <rwashington325@gmail.com>
2022-01-24 22:31:55 -0700

    Started building file structure for website

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+team_members = [
+    {
+        type: 'input',
+        message: 'Name of team member',
+        name: 'title'
+    },
+    {
+        type: 'list',
+        message: 'Teammates role',
+        name: 'role',
+        choices: ['Mananger', 'Engineer', 'Intern']
+    }
+]
+
+async function addTeammates(numTeam) {
+    const enteredTeam = []
+    for (let i = 0; i < numTeam; i++) {
+        await inquirer.prompt(team_members)
+        // Add logic to make sure you have all the information
+        .then((data) => enteredTeam.push(data))
+    }
+    console.log(enteredTeam)
+}
+
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'How many teammates?',
+            name: 'num'
+        }
+    ])
+        .then((data) => { addTeammates(parseInt(data.num)) })
+}
+
+init()
