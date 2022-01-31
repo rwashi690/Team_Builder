8705ebe90c97c9f68cea1fdc3bf382cad411d5d9
Rachel Washington <r@Rachels-Mac-mini.local>
2022-01-31 02:13:21 -0700

    start adding functionality for tests with jest

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -3,6 +3,10 @@ const Employee = require('../lib/employee.js');
 describe('Employee', ()=>{
     it ('Can set name through constructor', ()=>{
         const testE = new Employee ('Bob');
+        expect(testE.name).toBe('Bob');
+    })
+    it ('Can get name through method', ()=>{
+        const testE = new Employee ('Bob');
         expect(testE.getName()).toBe('Bob');
     })
 })
